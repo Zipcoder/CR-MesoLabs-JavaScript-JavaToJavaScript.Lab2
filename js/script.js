695c0db6afe04ca5ee3ca9d3800bc2767ab69694
pbattin <prestonbattin@Prestons-MacBook-Pro.local>
2017-02-01 15:23:05 -0500

    Finished lab

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 " use strict ";
+console.log("HEllo");
+
+var cat = {
+  speak: function(){
+    return "Meow";
+  }
+};
+
+var dog = {
+  speak: function(){
+    return "Woof";
+  }
+};
+
+var bird = {
+  speak: function(){
+    return "Tweet";
+  }
+};
+
 
 function PetChat(){
   var display = document.getElementById("display");
@@ -9,10 +29,27 @@ function PetChat(){
   };
 
   var askPetInfo = function(){
+
+    var talk;
     for(var i=0; i<numPets; i++){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
+
+      if(type.toUpperCase() === "CAT"){
+        talk = cat.speak();
+      }
+
+      if(type.toUpperCase() === "DOG"){
+        talk = dog.speak();
+      }
+
+      if(type.toUpperCase() === "BIRD"){
+        talk = bird.speak();
+      }
+      var textOut = type + " " + name + " goes " + talk + "<br />";
+
+
+
       display.innerHTML += textOut;
     }
   };
@@ -25,6 +62,7 @@ function PetChat(){
     askPetInfo();
   }
 };
+
 
 var chat = new PetChat();
 chat.init();
