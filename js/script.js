44e38427d7896047e6aa45bcb7956d81703eed42
Mark A Brown <markabrown87@gmail.com>
2017-05-24 18:50:17 -0400

    Finished Lab 2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,63 @@
 " use strict ";
 
+class Pet {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {}
+}
+
+class Dog extends Pet {
+  constructor(name) {
+    super(name);
+  }
+  speak() {
+    return "Woof!"
+  }
+}
+
+class Cat extends Pet {
+  constructor(name) {
+    super(name);
+  }
+  speak() {
+    return "Meow!"
+  }
+}
+
+class Bird extends Pet {
+  constructor(name) {
+    super(name);
+  }
+  speak() {
+    return "Chirp!"
+  }
+}
+
 function PetChat(){
   var display = document.getElementById("display");
   var numPets = null;
 
   var askHowMany = function(){
-    return prompt("How many pets do you have?");
+      return prompt("How many pets do you have?");
   };
 
   var askPetInfo = function(){
     for(var i=0; i<numPets; i++){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
+      if (type == "dog") {
+        var dog = new Dog(name);
+        var textOut = dog.name + " the " + type + " says " + dog.speak() + "<br />";
+      }
+      else if (type == "cat") {
+        var cat = new Cat(name);
+        var textOut = cat.name + " the " + type + " says " + cat.speak() + "<br />";
+      }
+      else {
+        var bird = new Bird(name);
+        var textOut = bird.name + " the " + type + " says " + bird.speak() + "<br />";
+      }
       display.innerHTML += textOut;
     }
   };
