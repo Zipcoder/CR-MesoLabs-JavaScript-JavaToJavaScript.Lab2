bc34387e65c6df805873d32c613db9e681939a51
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-05-24 22:18:39 -0400

    Objects laid out, started UI

--- js/script.js
+++ js/script.js
@@ -1,2 +1,49 @@
 " use strict ";
+var askPetType = prompt("Hey, what kinda pet you got?");
+switch(askPetType.toLowerCase()){
+  case "dog":
+  //TODO create new dog object
+  alert("You chose dog");
+  break;
+  case "cat":
+  //TODO create new cat object
+  alert("You chose cat");
+  break;
+  case "fish":
+  //TODO create new fish object
+  alert("You chose fish");
+  break;
+  default:
+  alert("You chose...poorly..try cat, fish, or dog next time");
+}
 
+class Pet {
+  constructor(name){
+    this.name = name;
+  }
+  speak(){
+    alert(this.name + " is a " + this.type + " and makes a noise");
+  }
+  getName(){
+    return this.name;
+  }
+  setName(){
+    return this.name;
+  }
+}
+class Dog extends Pet {
+
+  speak(){
+    alert(this.name + " is a dog and barks, all the damn time.");
+  }
+}
+class Cat extends Pet {
+  speak(){
+    alert(this.name + " is a cat and meows a lot.");
+  }
+}
+class Fish extends Pet {
+  speak(){
+    alert(this.name + " is a fish and blows bubbles.");
+  }
+}
