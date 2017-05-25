708718e14888933da81cd7000c14ef483f1e19d7
ant <anthonyjones@zipcoder3.local>
2017-05-24 20:08:00 -0400

    Finished for now

--- js/script.js
+++ js/script.js
@@ -1,29 +1,94 @@
 " use strict ";
 
-function PetChat(){
-  var display = document.getElementById("display");
-  var numPets = null;
 
-  var askHowMany = function(){
-    return prompt("How many pets do you have?");
-  };
 
-  var askPetInfo = function(){
-    for(var i=0; i<numPets; i++){
-      var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
-      var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
-      display.innerHTML += textOut;
+class Pet {
+
+    constructor(petName, petType) {
+        this.petName = petName;
+        this.petType = petType;
     }
-  };
-
-  this.init = function(){
-    while(numPets === null){
-      numPets = askHowMany();
+    speak() {
+        return "...I speak English";
     }
+}
 
-    askPetInfo();
-  }
+class Dog extends Pet {
+    constructor(petName, petType) {
+        super(petName, "Dog")
+    }
+    speak() {
+        super.speak();
+        return "*Ruff*";
+
+    }
+}
+
+class Cat extends Pet {
+    constructor(petName, petType) {
+        super(petName, "Cat")
+    }
+    speak() {
+        super.speak();
+        return "*Meow*";
+
+    }
+}
+
+class Bird extends Pet {
+    constructor(petName, petType) {
+        super(petName, "Bird")
+    }
+    speak() {
+        super.speak();
+        return "*Chirp*";
+
+    }
+}
+
+
+function PetChat() {
+    var display = document.getElementById("display");
+    var numPets = null;
+    var pet = new Pet();
+    var dog = new Dog();
+    var cat = new Cat();
+    var bird = new Bird();
+
+    var askHowMany = function () {
+        // console.log("How many pets do you have?");
+        return prompt("How many pets do you have?");
+    };
+
+    var askPetInfo = function () {
+        for (var i = 0; i < numPets; i++) {
+            var type = prompt("What is pet #" + (i + 1) + "? (cat, dog, bird)");
+
+            var name = prompt("What is pet #" + (i + 1) + "'s name'?");
+
+            if (type.toLowerCase() == "dog") {
+                pet = new Dog(name, type)
+                display.innerHTML += "You have a " + dog.petType + " named " + pet.petName[i].charAt(0).toUpperCase() + pet.petName.slice(1) + " " + dog.speak() + "<br>";
+            } else if (type.toLowerCase() == "cat") {
+                pet = new Cat(name, type)
+                display.innerHTML += "You have a " + cat.petType + " named " + pet.petName[i].charAt(0).toUpperCase() + pet.petName.slice(1) + " " + cat.speak() + "<br>";
+            } else if (type.toLowerCase() == "bird") {
+                pet = new Bird(name, type)
+                display.innerHTML += "You have a " + bird.petType + " named " + pet.petName[i].charAt(0).toUpperCase() + pet.petName.slice(1) + " " + bird.speak() + "<br>";
+            } else {
+                display.innerHTML += "You have a " + type + " named " + name[i].charAt(0).toUpperCase() + name.slice(1) + " " + pet.speak() + "<br>";
+            }
+
+        }
+    };
+
+    this.init = function () {
+        while (numPets === null) {
+            numPets = askHowMany();
+        }
+
+        askPetInfo();
+    }
 };
 
 var chat = new PetChat();
