dfbd1eb2520a42a4661960edeb0022a0172ac18e
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-05-25 08:17:42 -0400

    Display is wonky but logic is there

--- js/script.js
+++ js/script.js
@@ -1,20 +1,10 @@
 " use strict ";
-var askPetType = prompt("Hey, what kinda pet you got?");
-switch(askPetType.toLowerCase()){
-  case "dog":
-  //TODO create new dog object
-  alert("You chose dog");
-  break;
-  case "cat":
-  //TODO create new cat object
-  alert("You chose cat");
-  break;
-  case "fish":
-  //TODO create new fish object
-  alert("You chose fish");
-  break;
-  default:
-  alert("You chose...poorly..try cat, fish, or dog next time");
+
+function PetChat(){
+  askPetInfo();
+  var display = document.getElementById('display');
+  var numPets = null;
+
 }
 
 class Pet {
@@ -22,7 +12,7 @@ class Pet {
     this.name = name;
   }
   speak(){
-    alert(this.name + " is a " + this.type + " and makes a noise");
+    return this.name + " is a " + this.type + " and makes a noise";
   }
   getName(){
     return this.name;
@@ -32,18 +22,49 @@ class Pet {
   }
 }
 class Dog extends Pet {
-
+  constructor(name){
+  super(name);
+}
   speak(){
-    alert(this.name + " is a dog and barks, all the damn time.");
+    return "ruffff rarrrr barrrkkkk.";
   }
 }
 class Cat extends Pet {
+  constructor(name){
+  super(name);
+}
   speak(){
-    alert(this.name + " is a cat and meows a lot.");
+    return "meeeooowwww.";
   }
 }
 class Fish extends Pet {
+  constructor(name){
+  super(name);
+}
   speak(){
-    alert(this.name + " is a fish and blows bubbles.");
+    return "bubble bubble bubble!.";
   }
 }
+
+var askPetInfo = function(){
+  var result = "";
+   for(var i=0; i < numPets; i++){
+     var type = prompt("What kind of pet is pet " + (i+1) + "? (dog, cat, fish would be good picks)");
+     var name = prompt("What do you call pet" + (i+1));
+
+      if (type == "dog") {
+        var dog = new Dog(name);
+        var textOutput = dog.getName() + " is a  " + this.type + " that says " + dog.speak() + "<br />";
+      }
+      else if (type == "cat") {
+        var cat = new Cat(name);
+        var textOutput = cat.getName() + " the " + this.type + " that says " + cat.speak() + "<br />";
+      }
+      else {
+        var fish = new Fish(name);
+        var textOutput = fish.getName() + " the " + this.type + " that says " + fish.speak() + "<br />";
+      }
+   }
+   result += textOutput;
+   return result;
+ }
