44dfa00fdf7f8976514e06dc298e9423af5122f8
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-27 16:40:53 -0400

    Fixed it, all done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 " use strict ";
 
+class Pet {
+  constructor(name) { ///,type
+    this.name = name;
+    //this.type = type;
+  }
+  getName(){
+    return this.name;
+  }
+
+  setName(){
+    this.name = name;
+  }
+
+  speak(){
+
+  }
+}
+class Cat extends Pet {
+  constructor(name) {
+    super(name);
+  }
+  speak(){
+    return "Meow.";
+  }
+}
+class Dog extends Pet {
+  constructor(name) {
+    super(name);
+  }
+  speak(){
+    return "Bjorke!";
+  }
+}
+class Bird extends Pet {
+  constructor(name) {
+    super(name);
+  }
+  speak(){
+    return "SCREEEEECHHH!!!111";
+  }
+}
+
+
 function PetChat(){
   var display = document.getElementById("display");
   var numPets = null;
@@ -13,20 +56,23 @@ function PetChat(){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
       var textOut = type + " " + name + "<br />";
+      //display.innerHTML += textOut;
+
+      if (type.toLowerCase() === "cat") {
+        var cat = new Cat(name);
+        textOut += "Your " + type + ", " + name + ", says " + cat.speak() + "<br/>";
+      }
+
+      else if(type.toLowerCase() === "dog") {
+        var dog = new Dog(name);
+        textOut += "Your " + type + ", " + name + ", says " + dog.speak() + "<br/>";
+      }
+
+      else if(type.toLowerCase() === "bird") {
+        var bird = new Bird(name);
+        textOut += "Your " + type + ", " + name + ", says " + bird.speak() + "<br/>";
+      }
       display.innerHTML += textOut;
-
-      if (type.toLowerCase() == "cat"){
-        pet = new Cat(name);
-        
-      }
-
-      else if(type.toLowerCase() == "dog"){
-        pet = new Dog(name);
-      }
-
-      else if(type.toLowerCase() == "bird"){
-        pet = new Bird(name);
-      }
       //display.innerHTML += "Your " + type + ", " + pet.getName() + "," " says " + pet.speak() + "<br/>";
     }
   };
@@ -42,48 +88,3 @@ function PetChat(){
 
 var chat = new PetChat();
 chat.init();
-
-class Pet {
-  constructor(name) { ///,type
-    this.name = name;
-    //this.type = type;
-  }
-  getName(){
-    return this.name;
-  }
-
-  // setName(){
-  //   this.name = name;
-  // }
-
-  speak(){
-
-  }
-}
-class Cat extends Pet {
-  constructor(name) {
-    super(name);
-  }
-  //super(name);
-  speak(){
-    return "Meow.";
-  }
-}
-class Dog extends Pet {
-  constructor(name) {
-    super(name);
-  }
-  //super(name);
-  speak(){
-    return "Bjorke!";
-  }
-}
-class Bird extends Pet {
-  constructor(name) {
-    super(name);
-  }
-  //super(name);
-  speak(){
-    return "SCREEEEECHHH!!!111";
-  }
-}
